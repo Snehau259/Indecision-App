66f77b9f90b1a04638aebdbf639c9ebabb75dd73
Snehau259 <snehau259@gmail.com>
2022-09-26 22:35:48 +0530

    [Sneha] jsx expressions using objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,27 @@
-'use strict';
+"use strict";
 
 //contains jsx
 console.log('app.js is running');
 // var template= <p>This is template from app.js</p>
 // var template = (<div><p> This is template from app.js</p> <h1> hello</h1><p>okbie</p> </div>);
-var userName = 'Sneha U';
-var userAge = 21;
-var userLocation = 'India';
+// var userName='Sneha U';
+// var userAge=21;
+// var userLocation='India';
+var app = { title: "This is the title", subtitle: "This is the subtitle" };
 var templateTwo = React.createElement(
-  'div',
+  "div",
   null,
   React.createElement(
-    'h1',
+    "h1",
     null,
-    userName
+    "Title:",
+    app.title
   ),
   React.createElement(
-    'p',
+    "p",
     null,
-    'Age:',
-    userAge
-  ),
-  React.createElement(
-    'p',
-    null,
-    'Location:',
-    userLocation
+    "Subtitle:",
+    app.subtitle
   )
 );
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@
 console.log('app.js is running')
 // var template= <p>This is template from app.js</p>
 // var template = (<div><p> This is template from app.js</p> <h1> hello</h1><p>okbie</p> </div>);
-var userName='Sneha U';
-var userAge=21;
-var userLocation='India';
-var templateTwo=(<div><h1>{userName}</h1><p>Age:{userAge}</p><p>Location:{userLocation}</p></div>);
+// var userName='Sneha U';
+// var userAge=21;
+// var userLocation='India';
+var app={title:"This is the title", subtitle:"This is the subtitle"};
+var templateTwo=(<div><h1>Title:{app.title}</h1><p>Subtitle:{app.subtitle}</p></div>);
 var appRoot=document.getElementById('app')
 ReactDOM.render(templateTwo,appRoot);
