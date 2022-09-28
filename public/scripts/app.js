4657a6edd1c029dc0a6ee1bbb65a2986ca67d3d5
Snehau259 <snehau259@gmail.com>
2022-09-28 16:07:22 +0530

    [sneha] arrow functions part 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,69 @@
-'use strict';
+"use strict";
 
-var square = function square(x) {
-  return x * x;
+var add = function add(a, b) {
+    console.log(arguments); //Arguments(2) [2, 3, callee: (...), Symbol(Symbol.iterator): ƒ]
+    return a + b;
 };
-console.log(square(8));
+console.log(add(2, 3));
 
-var squareArrow = function squareArrow(x) {
-  return x * x;
+//ARGUEMENTS OBJECT NO LONGER BOUND WITH ARROW FUNCTIONS
+var addArrow = function addArrow(a, b)
+// {console.log(arguments);app.js:3 Uncaught ReferenceError: arguments is not defined
+{
+    return a + b;
 };
-console.log(squareArrow(9));
+console.log(add(2, 3));
 
-var squareArrowPrecise = function squareArrowPrecise(x) {
-  return x * x;
-};
-console.log(squareArrowPrecise(10));
+//THIS KEYWORD NO LONGER BOUND
 
-var getFirstName = function getFirstName(fullname) {
-  return fullname.split(' ')[0];
-};
-console.log(getFirstName("Sneha U"));
 
-var getFirstName2 = function getFirstName2(fullname) {
-  fullname.split(' ')[0];
+/* const user={name:"Sneha",
+cities:["palakkad","kochi","kanhangad"],
+printPlacesLived:function()
+{
+    console.log(this.name);
+    console.log(this.cities);
+
+    this.cities.forEach(function(city)
+    {
+        console.log(this.name+" has lived in "+ city)// app.js:24 Uncaught TypeError: Cannot read properties of undefined (reading 'name')
+    })
+}}
+user.printPlacesLived(); */
+
+var user = { name: "Sneha",
+    cities: ["palakkad", "kochi", "kanhangad"],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        this.cities.forEach(function (city) {
+            console.log(_this.name + " has lived in " + city);
+        });
+    } };
+user.printPlacesLived();
+
+var userMap = {
+    name: "Sneha",
+    cities: ["palakkad", "kochi", "kanhangad"],
+    printPlacesLived: function printPlacesLived() {
+        var _this2 = this;
+
+        return this.cities.map(function (city) {
+            return _this2.name + " has lived in " + city;
+        });
+    }
 };
-console.log(getFirstName("Sneha U"));
+console.log(userMap.printPlacesLived());
+
+var multiplier = {
+    numbers: [1, 2, 3, 4, 5],
+    multiplyBy: 10,
+    multiply: function multiply() {
+        var _this3 = this;
+
+        return this.numbers.map(function (num) {
+            return num * _this3.multiplyBy;
+        });
+    }
+};
+console.log(multiplier.multiply());
---
+++ src/playground/es6-arrow-function2.js
@@ -0,0 +1,70 @@
+const add=function(a,b)
+{console.log(arguments);//Arguments(2) [2, 3, callee: (...), Symbol(Symbol.iterator): ƒ]
+    return a+b;
+}
+console.log(add(2,3))
+
+//ARGUEMENTS OBJECT NO LONGER BOUND WITH ARROW FUNCTIONS
+const addArrow=(a,b)=>
+// {console.log(arguments);app.js:3 Uncaught ReferenceError: arguments is not defined
+    {return a+b;
+}
+console.log(add(2,3))
+
+//THIS KEYWORD NO LONGER BOUND
+
+
+/* const user={name:"Sneha",
+cities:["palakkad","kochi","kanhangad"],
+printPlacesLived:function()
+{
+    console.log(this.name);
+    console.log(this.cities);
+
+    this.cities.forEach(function(city)
+    {
+        console.log(this.name+" has lived in "+ city)// app.js:24 Uncaught TypeError: Cannot read properties of undefined (reading 'name')
+    })
+}}
+user.printPlacesLived(); */
+
+
+const user={name:"Sneha",
+cities:["palakkad","kochi","kanhangad"],
+printPlacesLived:function()
+{
+   
+
+    this.cities.forEach((city)=>
+    {
+        console.log(this.name+" has lived in "+ city)
+    });
+}}
+user.printPlacesLived();
+
+const userMap={
+    name:"Sneha",
+cities:["palakkad","kochi","kanhangad"],
+printPlacesLived()
+{
+   
+
+   return this.cities.map((city)=>
+    {
+        return (this.name+" has lived in "+ city);
+    });
+}
+};
+console.log(userMap.printPlacesLived());
+
+const multiplier={
+    numbers:[1,2,3,4,5],
+    multiplyBy:10,
+    multiply()
+    {
+        return this.numbers.map((num)=>num*this.multiplyBy)
+    }
+}
+console.log(multiplier.multiply());
+
+
