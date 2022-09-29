5ec0585017ebd064d3dd750d1dd0873299276810
Snehau259 <snehau259@gmail.com>
2022-09-29 08:15:26 +0530

    [sneha] events and attributes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,69 +1,44 @@
-"use strict";
+'use strict';
 
-var add = function add(a, b) {
-    console.log(arguments); //Arguments(2) [2, 3, callee: (...), Symbol(Symbol.iterator): ƒ]
-    return a + b;
+//contains jsx
+console.log('app.js is running');
+
+var count = 0;
+var addOne = function addOne() {
+  console.log('added one');
 };
-console.log(add(2, 3));
-
-//ARGUEMENTS OBJECT NO LONGER BOUND WITH ARROW FUNCTIONS
-var addArrow = function addArrow(a, b)
-// {console.log(arguments);app.js:3 Uncaught ReferenceError: arguments is not defined
-{
-    return a + b;
+var minusOne = function minusOne() {
+  console.log('subtracted one');
 };
-console.log(add(2, 3));
-
-//THIS KEYWORD NO LONGER BOUND
-
-
-/* const user={name:"Sneha",
-cities:["palakkad","kochi","kanhangad"],
-printPlacesLived:function()
-{
-    console.log(this.name);
-    console.log(this.cities);
-
-    this.cities.forEach(function(city)
-    {
-        console.log(this.name+" has lived in "+ city)// app.js:24 Uncaught TypeError: Cannot read properties of undefined (reading 'name')
-    })
-}}
-user.printPlacesLived(); */
-
-var user = { name: "Sneha",
-    cities: ["palakkad", "kochi", "kanhangad"],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        this.cities.forEach(function (city) {
-            console.log(_this.name + " has lived in " + city);
-        });
-    } };
-user.printPlacesLived();
-
-var userMap = {
-    name: "Sneha",
-    cities: ["palakkad", "kochi", "kanhangad"],
-    printPlacesLived: function printPlacesLived() {
-        var _this2 = this;
-
-        return this.cities.map(function (city) {
-            return _this2.name + " has lived in " + city;
-        });
-    }
+var reset = function reset() {
+  console.log('reset');
 };
-console.log(userMap.printPlacesLived());
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Count:',
+    count
+  ),
+  React.createElement(
+    'button',
+    { id: 'my-id', className: 'button', onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'reset'
+  )
+);
 
-var multiplier = {
-    numbers: [1, 2, 3, 4, 5],
-    multiplyBy: 10,
-    multiply: function multiply() {
-        var _this3 = this;
-
-        return this.numbers.map(function (num) {
-            return num * _this3.multiplyBy;
-        });
-    }
-};
-console.log(multiplier.multiply());
+console.log(templateTwo);
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 //contains jsx
 console.log('app.js is running')
-// const template= <p>This is template from app.js</p>
-// const template = (<div><p> This is template from app.js</p> <h1> hello</h1><p>okbie</p> </div>);
-// const userName='Sneha U';
-// const userAge=21;
-// const userLocation='India';
-const app = { title: "This is the title", subtitle: "This is the subtitle" ,options:["one","two","three"]};
-const user = { name: "sneha", age: 22, location: "India" };
-const template = (<div>
-    <h1>Name:{user.name ? user.name : "anonymus"}</h1>
-    {user.age>=18 && <p>Age:{user.age}</p>}
-    {getLocation(user.location)}
-</div>);
-const templateTwo = (<div><h1>Title:{app.title}</h1>
-{app.subtitle && <p>Subtitle:{app.subtitle}</p>}
-<p>{app.options.length>0? 'Here are your options' : 'No options'}</p>
-</div>);
+
+let count=0
+const addOne=() => {console.log('added one')};
+const minusOne=() => {console.log('subtracted one')};
+const reset=() => {console.log('reset')};
+const templateTwo=(<div>
+<h1>Count:{count}</h1>
+
+<button id='my-id' className="button" onClick={addOne}>+1</button>
+<button  onClick={minusOne}>-1</button>
+<button onClick={reset}>reset</button></div>);
+
+
+console.log(templateTwo)
 const appRoot = document.getElementById('app')
 ReactDOM.render(templateTwo, appRoot);
 
-function getLocation(location) {
-    if (location)
-        return <p>location:{location}</p>
-    // else return 'unknown'
-} 
+
