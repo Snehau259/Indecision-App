040e39ca43a67c9c8eec0421a8ab6212774acd8a
Snehau259 <snehau259@gmail.com>
2022-09-30 18:06:09 +0530

    [sneha] ES6 classes part 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -15,6 +15,40 @@ getDescription()
 
 }
 }
+class Student extends Person{
+    constructor (name,age,major)
+    {
+        console.log('student constructor called')
+        super(name,age);
+        this.major=major;
+    }
+    hasMajor()
+    {return `${!!this.major}`}
+    getDescription(){
+        let description=super.getDescription()
+        if(this.hasMajor())
+        description+=`The major is ${this.major}`
+        return description;
+
+    }
+}
+
+class Traveller extends Person{
+    constructor(name,age,homeLocation)
+    {
+        super(name,age);
+        this.homeLocation=homeLocation;
+    }
+    getGreetings()
+    {
+        let greetings=super.getGreetings()
+        if (this.homeLocation)
+        {
+            greetings+=`I am visitng from ${this.homeLocation}`
+        }
+        return greetings;
+    }
+}
 
 
 const me=new Person('Sneha',22);
@@ -24,3 +58,13 @@ console.log(me.getDescription())
 const other= new Person();
 console.log(other.getGreetings());
 console.log(other.getDescription())
+
+const meStudent=new Student('Sruthi',22,'c');
+console.log(meStudent.getGreetings());
+console.log(meStudent.getDescription())
+console.log(meStudent.hasMajor())
+
+const meTraveller=new Traveller('SnehaTraveller',22,'Kerala');
+console.log(meTraveller.getGreetings());
+console.log(meTraveller.getDescription())
+// console.log(meTraveller.hasMajor())
