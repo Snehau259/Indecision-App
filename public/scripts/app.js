08ffd4f9d2ee635fb12bef3bffb274922e0d3ed7
Snehau259 <snehau259@gmail.com>
2022-09-30 12:07:43 +0530

    [sneha] visibility toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,90 +1,41 @@
-"use strict";
+'use strict';
 
-var app = { title: "Indecision App", subtitle: "Put your life in the hands of a computer", options: [] };
-// const templateTwo=(<div>
-//     <h1></h1>
-//     <p></p>
-//     </div>);
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
+var visibility = false;
+var showDetails = function showDetails() {
+    console.log('clicked');
+    visibility = !visibility;
 
-  /* The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-   For example, this can be useful when:
-  
-  Clicking on a "Submit" button, prevent it from submitting a form
-  Clicking on a link, prevent the link from following the URL
-  
-  in this case we need no data to be added to the url
-  */
-  var formInput = e.target.option.value;
-  if (formInput) {
-    app.options.push(formInput);
-    e.target.option.value = '';
-  }
-  console.log('form submitted');
-  renderForm();
-};
-//const numbers=[1,2,3,4,5]
-var removeAll = function removeAll() {
-  app.options = [];
-  renderForm();
-};
-var makeDecision = function makeDecision() {
-  var randNum = Math.floor(Math.random() * app.options.length);
-  alert(app.options[randNum]);
+    // return <p>here are the details</p>
+    // ReactDOM.render(template, appRoot);
+    renderPage();
 };
 
-function renderForm() {
-  var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h1",
-      null,
-      "Title:",
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      "p",
-      null,
-      "Subtitle:",
-      app.subtitle
-    ),
-    React.createElement(
-      "button",
-      { onClick: removeAll },
-      "Remove all"
-    ),
-    React.createElement(
-      "p",
-      null,
-      app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-      "button",
-      { disabled: app.options.length === 0, onClick: makeDecision },
-      "What should i do?"
-    ),
-    app.options.map(function (item) {
-      return React.createElement(
-        "li",
-        { key: item },
-        item
-      );
-    }),
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { type: "text", name: "option" }),
-      React.createElement(
-        "button",
+var renderPage = function renderPage() {
+    var template = React.createElement(
+        'div',
         null,
-        "Add option"
-      )
-    )
-  );
-  ReactDOM.render(templateTwo, appRoot);
-}
+        React.createElement(
+            'h1',
+            null,
+            'Visibility Toggle'
+        ),
+        React.createElement(
+            'button',
+            { onClick: showDetails },
+            visibility ? 'hide details' : 'show details'
+        ),
+        visibility && React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'p',
+                null,
+                'here are your details'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
 
 var appRoot = document.getElementById('app');
-renderForm();
+renderPage();
---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,25 @@
+let visibility=false;
+const showDetails=()=>{console.log('clicked')
+visibility=!visibility;
+
+    // return <p>here are the details</p>
+    // ReactDOM.render(template, appRoot);
+renderPage();}
+    
+
+
+
+
+
+const renderPage=()=>{
+    const template=(<div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={showDetails}>{visibility?'hide details':'show details'}</button>
+        {visibility && (<div><p>here are your details</p></div>)}
+        </div>)
+        ReactDOM.render(template, appRoot);
+}
+
+
+const appRoot = document.getElementById('app')
+renderPage();
