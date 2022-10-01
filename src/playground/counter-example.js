2fb2c9f85f687e747846fbe662bdbf84683461de
Snehau259 <snehau259@gmail.com>
2022-10-01 15:03:00 +0530

    [sneha] events and method

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,19 +1,22 @@
 //contains jsx
 console.log('app.js is running')
 
-let count=0
-const addOne=() => {
+let count = 0
+const addOne = () => {
     count++;
-    console.log('added one',count)
-    renderCounterApp();};
-const minusOne=() => {
+    console.log('added one', count)
+    renderCounterApp();
+};
+const minusOne = () => {
     count--;
     console.log('subtracted one')
-    renderCounterApp();};
-const reset=() => {
-    count=0;
+    renderCounterApp();
+};
+const reset = () => {
+    count = 0;
     console.log('reset')
-    renderCounterApp();};
+    renderCounterApp();
+};
 
 
 
@@ -22,15 +25,14 @@ const appRoot = document.getElementById('app')
 
 
 
-function renderCounterApp()
-{
-    const templateTwo=(<div>
+function renderCounterApp() {
+    const templateTwo = (<div>
         <h1>Count:{count}</h1>
-        
+
         <button id='my-id' className="button" onClick={addOne}>+1</button>
-        <button  onClick={minusOne}>-1</button>
+        <button onClick={minusOne}>-1</button>
         <button onClick={reset}>reset</button></div>);
-        ReactDOM.render(templateTwo, appRoot);
+    ReactDOM.render(templateTwo, appRoot);
 }
 
 renderCounterApp();
